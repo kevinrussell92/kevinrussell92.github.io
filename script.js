b789b34d20d70648cb25a4e4328685dd17b577a4
kevinrussell92 <kevinrussell92@hotmail.com>
2026-07-30 11:56:34 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e){
- e.preventDefault();
- document.getElementById("message").textContent="Thank you. Your inquiry has been received.";
- this.reset();
-});
